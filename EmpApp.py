b7c5bc19e7744ab3ab9002402e11064a3e5cbec9
ScorpioAng <113013045+ScorpioAng@users.noreply.github.com>
2022-09-25 17:21:17 +0800

    da

--- EmpApp.py
+++ EmpApp.py
@@ -30,6 +30,10 @@ headings=("emp_id","emp_email","emp_name","emp_DoB","emp_contact", "emp_departme
 def home():
     return render_template('index.html')
 
+@app.route("/templates/add-employee.html", methods=['GET'])
+    def ViewEmp():
+        return render_template('add-employee.html')
+
 @app.route("/templates/add-employee.html", methods=['GET','POST'])
 def AddEmp():
     emp_id = request.form['emp_id']
